82c5e65b85eac945c1685991346154e3899ad551
Paul Walsh <twentytwentyk@live.com>
2017-09-07 14:49:20 -0400

    receipt

--- app.js
+++ app.js
@@ -87,9 +87,9 @@ function receivedMessage(event) {
       case 'button':
         sendButtonMessage(senderID);
         break;
-      // case 'receipt':
-      //   sendReceiptMessage(senderID);
-      //   break;
+      case 'receipt':
+        sendReceiptMessage(senderID);
+        break;
       default:
         sendTextMessage(senderID, messageText);
     }
@@ -97,70 +97,72 @@ function receivedMessage(event) {
     sendTextMessage(senderID, "Message with attachment received");
   }
 }
- 
-//
-// function sendReceiptMessage(recipientId, messageText) {
-//   var messageData = {
-//     recipient: recipientId,
-//   },
-//     message:{
-//     attachment:{
-//       type:"template",
-//       payload:{
-//         template_type:"receipt",
-//         recipient_name:"Stephane Crozatier",
-//         order_number:"12345678902",
-//         currency:"USD",
-//         payment_method:"Visa 2345",
-//         order_url:"http://petersapparel.parseapp.com/order?order_id=123456",
-//         timestamp:"1428444852",
-//         address:{
-//           street_1:"1 Hacker Way",
-//           street_2:"",
-//           city:"Menlo Park",
-//           postal_code:"94025",
-//           state:"CA",
-//           country:"US"
-//         },
-//         summary:{
-//           subtotal:75.00,
-//           shipping_cost:4.95,
-//           total_tax:6.19,
-//           total_cost:56.14
-//         },
-//         adjustments:[
-//           {
-//             name:"New Customer Discount",
-//             amount:20
-//           },
-//           {
-//             name:"$10 Off Coupon",
-//             amount:10
-//           }
-//         ],
-//         elements:[
-//           {
-//             title:"Classic White T-Shirt",
-//             subtitle:"100% Soft and Luxurious Cotton",
-//             quantity:2,
-//             price:50,
-//             currency:"USD",
-//             image_url:"http://petersapparel.parseapp.com/img/whiteshirt.png"
-//           },
-//           {
-//             title:"Classic Gray T-Shirt",
-//             subtitle:"100% Soft and Luxurious Cotton",
-//             quantity:1,
-//             price:25,
-//             currency:"USD",
-//             image_url:"http://petersapparel.parseapp.com/img/grayshirt.png"
-//           }
-//         ]
-//       }
-//     }
-//   };
-//   callSendAPI(messageData);
-// }
+
+
+function sendReceiptMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId,
+    },
+    message: {
+      attachment:{
+        type:"template",
+        payload:{
+          template_type:"receipt",
+          recipient_name:"Stephane Crozatier",
+          order_number:"12345678902",
+          currency:"USD",
+          payment_method:"Visa 2345",
+          order_url:"http://petersapparel.parseapp.com/order?order_id=123456",
+          timestamp:"1428444852",
+          address:{
+            street_1:"1 Hacker Way",
+            street_2:"",
+            city:"Menlo Park",
+            postal_code:"94025",
+            state:"CA",
+            country:"US"
+          },
+          summary:{
+            subtotal:75.00,
+            shipping_cost:4.95,
+            total_tax:6.19,
+            total_cost:56.14
+          },
+          adjustments:[
+            {
+              name:"New Customer Discount",
+              amount:20
+            },
+            {
+              name:"$10 Off Coupon",
+              amount:10
+            }
+          ],
+          elements:[
+            {
+              title:"Classic White T-Shirt",
+              subtitle:"100% Soft and Luxurious Cotton",
+              quantity:2,
+              price:50,
+              currency:"USD",
+              image_url:"http://petersapparel.parseapp.com/img/whiteshirt.png"
+            },
+            {
+              title:"Classic Gray T-Shirt",
+              subtitle:"100% Soft and Luxurious Cotton",
+              quantity:1,
+              price:25,
+              currency:"USD",
+              image_url:"http://petersapparel.parseapp.com/img/grayshirt.png"
+            }
+          ]
+        }
+      }
+    }
+  };
+  callSendAPI(messageData);
+}
 
 function sendGenericMessage(recipientId, messageText) {
   var messageData = {

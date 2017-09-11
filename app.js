00a3fe58860093e312fceb7ef92ac688141289fa
Paul Walsh <twentytwentyk@live.com>
2017-09-11 15:48:42 -0400

    test 2000

--- app.js
+++ app.js
@@ -104,65 +104,29 @@ function sendReceiptMessage(recipientId, messageText) {
     recipient: {
       id: recipientId,
     },
-    message: {
+    message:{
       attachment:{
         type:"template",
         payload:{
-          template_type:"receipt",
-          recipient_name:"Stephane Crozatier",
-          order_number:"12345678902",
-          currency:"USD",
-          payment_method:"Visa 2345",
-          order_url:"http://petersapparel.parseapp.com/order?order_id=123456",
-          timestamp:"1428444852",
-          address:{
-            street_1:"1 Hacker Way",
-            street_2:"",
-            city:"Menlo Park",
-            postal_code:"94025",
-            state:"CA",
-            country:"US"
-          },
-          summary:{
-            subtotal:75.00,
-            shipping_cost:4.95,
-            total_tax:6.19,
-            total_cost:56.14
-          },
-          adjustments:[
-            {
-              name:"New Customer Discount",
-              amount:20
-            },
-            {
-              name:"$10 Off Coupon",
-              amount:10
-            }
-          ],
+          template_type:"open_graph",
           elements:[
-            {
-              title:"Classic White T-Shirt",
-              subtitle:"100% Soft and Luxurious Cotton",
-              quantity:2,
-              price:50,
-              currency:"USD",
-              image_url:"http://petersapparel.parseapp.com/img/whiteshirt.png"
-            },
-            {
-              title:"Classic Gray T-Shirt",
-              subtitle:"100% Soft and Luxurious Cotton",
-              quantity:1,
-              price:25,
-              currency:"USD",
-              image_url:"http://petersapparel.parseapp.com/img/grayshirt.png"
+             {
+              url:"https://open.spotify.com/track/7GhIk7Il098yCjg4BQjzvb",
+              buttons:[
+                {
+                  type:"web_url",
+                  url:"https://en.wikipedia.org/wiki/Rickrolling",
+                  title:"View More",
+                }
+              ]
             }
           ]
         }
       }
     }
-  };
+  }
   callSendAPI(messageData);
-}
+};
 
 function sendGenericMessage(recipientId, messageText) {
   var messageData = {
@@ -198,7 +162,7 @@ function sendTextMessage(recipientId, messageText) {
       id: recipientId
     },
     message: {
-      text: messageText
+      text: "the last message sent was: " + messageText,
     }
   };
 
